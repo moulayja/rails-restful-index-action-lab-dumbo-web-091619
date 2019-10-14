b92fe2a9a727ca803b776162a8215a989506f707
Jamal Farah <moulayjam@gmail.com>
2019-10-14 13:23:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+      private  
+    def post_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
 
 end
 
